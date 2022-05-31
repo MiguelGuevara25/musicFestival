c33e79f9013e720a97e820ecc22fc64fbadaa3aa
MiguelGuevara25 <miguel_g1789@hotmail.com>
2022-05-31 14:41:23 -0500

    new change

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 const {
     src,
     dest,
-    watch
+    watch,
+    parallel
 } = require("gulp");
+
+//!CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require("gulp-plumber");
+
+//!IMÁGENES
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function css(done) {
 
@@ -17,6 +25,31 @@ function css(done) {
     done(); //*Callback que avisa a gulp cuando llegamos al final
 }
 
+function imagenes(done) {
+
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'));
+
+    done();
+}
+
+function versionWebp(done) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+
+    done();
+}
+
 function dev(done) {
 
     watch('src/scss/**/*.scss', css);
@@ -25,4 +58,6 @@ function dev(done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes
+exports.versionWebp = versionWebp
+exports.dev = parallel(imagenes, versionWebp, dev);
